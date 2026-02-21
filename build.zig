aa1d87f5611f89d62d11eb4dd75e61fa1fb7fd8f
Jassiel Ovando <jassielovando@protonmail.com>
2026-02-20 23:37:25 -0400

    fix docs

--- build.zig
+++ build.zig
@@ -3,21 +3,24 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const name: []const u8 = "toonz";
+    const mod_name: []const u8 = "toonz";
 
-    const mod = b.addModule(name, .{
+    const lib_mod = b.addModule(mod_name, .{
         .root_source_file = b.path("src/lib/root.zig"),
         .target = target,
+        .optimize = optimize,
     });
 
+    const lib = b.addLibrary(.{ .name = mod_name, .root_module = lib_mod });
+
     const exe = b.addExecutable(.{
-        .name = name,
+        .name = mod_name,
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/cli/main.zig"),
             .target = target,
             .optimize = optimize,
             .imports = &.{
-                .{ .name = name, .module = mod },
+                .{ .name = mod_name, .module = lib_mod },
             },
         }),
     });
@@ -36,14 +39,14 @@ pub fn build(b: *std.Build) void {
     }
 
     const docs = b.addInstallDirectory(.{
-        .source_dir = exe.getEmittedDocs(),
+        .source_dir = lib.getEmittedDocs(),
         .install_dir = .prefix,
         .install_subdir = "docs",
     });
     const docs_step = b.step("docs", "Generate documentation");
     docs_step.dependOn(&docs.step);
 
-    const tests = b.addTest(.{ .root_module = b.createModule(.{ .root_source_file = b.path("tests/suite.zig"), .target = target, .optimize = optimize, .imports = &.{.{ .name = name, .module = mod }} }) });
+    const tests = b.addTest(.{ .root_module = b.createModule(.{ .root_source_file = b.path("tests/suite.zig"), .target = target, .optimize = optimize, .imports = &.{.{ .name = mod_name, .module = lib_mod }} }) });
     const run_tests = b.addRunArtifact(tests);
 
     const test_step = b.step("tests", "Run tests");
